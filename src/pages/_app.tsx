c72d1bd44ad46fc12cbae8dcdfdae1b93d43651f
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-05-05 16:21:00 -0400

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import {Amplify} from "aws-amplify";
+import awsExports from "../aws-exports";
+import type {AppProps} from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+Amplify.configure({...awsExports, ssr: true});
+
+export default function App({Component, pageProps}: AppProps) {
+  // @ts-ignore
   return <Component {...pageProps} />
 }
