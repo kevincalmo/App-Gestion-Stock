e3e6647e103318219b8906093e5e9f74c28ebcb7
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-05-07 08:28:07 -0400

    Add logic to create user inside datastore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,74 @@
 import "@/styles/globals.css";
-import {Amplify} from "aws-amplify";
+import {Amplify, withSSRContext, DataStore, AuthModeStrategyType} from "aws-amplify";
 import awsExports from "../aws-exports";
 import type {AppProps} from "next/app";
+import {withAuthenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import {useEffect, useState} from "react";
+import {User} from "@/models";
+import {Center, ChakraProvider, Spinner} from "@chakra-ui/react";
 
-Amplify.configure({...awsExports, ssr: true});
+Amplify.configure({
+    ...awsExports,
+    ssr: true,
+});
 
-export default function App({Component, pageProps}: AppProps) {
-  // @ts-ignore
-  return <Component {...pageProps} />
+function App({Component, pageProps}: AppProps) {
+    const [isLoading, setIsLoading] = useState(false);
+    let n = 0;
+    useEffect(() => {
+        if (n === 0) {
+            (async function () {
+                setIsLoading(true);
+                try {
+
+                    const auth = await Amplify.Auth.currentAuthenticatedUser();
+                    const userDb = await DataStore.query(User, (u) => u.sub.eq(auth.attributes.sub));
+                    if (userDb.length === 0) {
+                        await DataStore.save(
+                            new User({
+                                // @ts-ignore
+                                name: auth.attributes.name,
+                                email: auth.attributes.email,
+                                isAdmin: false,
+                                sub: auth.attributes.sub
+                            })
+                        );
+                        console.log("User create !");
+                        setIsLoading(false);
+                    } else {
+                        console.log("User already exist !");
+                        setIsLoading(false);
+                    }
+                } catch (e) {
+                    setIsLoading(false);
+                    console.log("ERROR : " + e);
+                }
+            })();
+            n++;
+        }
+
+    }, [n]);
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const value = Object.fromEntries(data.entries());
+        console.log(value);
+    };
+    if (isLoading) return (<Center h={"100%"}>
+        <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+        />
+    </Center>);
+
+    // @ts-ignore
+    return <ChakraProvider><Component {...pageProps} /></ChakraProvider>;
 }
+
+export default withAuthenticator(App);
+
