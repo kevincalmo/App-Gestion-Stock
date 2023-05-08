82c84cdd4d90c9e0f60defcfebcc29a947898e2e
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-05-08 15:24:53 -0400

    Create a HomeHeading component to the home page

---
+++ src/components/HomeHeading.tsx
@@ -0,0 +1,41 @@
+import {Box, Center, Flex, Heading, Stack} from "@chakra-ui/react";
+
+const HomeHeading = ({title = "Titre de rubrique", content = <>Hello</>}: { title: string, content: any }) => {
+    return (
+        <Box
+            width={{base: "100%"}}
+            height={{base: "155px"}}
+            bg="white"
+            boxShadow="lg"
+            rounded="sm"
+        >
+            <Stack
+                height='100%'
+            >
+                <Box
+                    bg="blue.500"
+                    width="100%"
+                    height="100px"
+                >
+                    <Center
+                        h="100%"
+                    >
+                        <Heading
+                            textAlign="center"
+                        >{title}</Heading>
+                    </Center>
+                </Box>
+                <Box
+                height='100%'
+                >
+                    <Center
+                        h="100%"
+                    >
+                        {content}
+                    </Center>
+                </Box>
+            </Stack>
+        </Box>
+    );
+};
+export default HomeHeading;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,16 @@ import Layout from "@/layout";
 import {Amplify, withSSRContext} from "aws-amplify";
 import {serializeModel} from "@aws-amplify/datastore/ssr";
 import {Stock, User, UserStock} from "@/models";
+import HomeHeading from "@/components/HomeHeading";
 
 export default function Home({user,stocks}:any) {
   console.log(user);
   console.log(stocks);
     return (
         <Layout>
-          {user.sub}
+            <HomeHeading
+                title={"Listes de produits"}
+                content={<>12</>}/>
         </Layout>
     );
 }
@@ -17,17 +20,17 @@ export default function Home({user,stocks}:any) {
 export async function getServerSideProps({req}: { req: any }) {
     const SSR = withSSRContext({req});
     const userAuth = await SSR.Auth.currentAuthenticatedUser();
-    const userDb = await SSR.DataStore.query(User, (u:any) => u.sub.eq(userAuth.attributes.sub));
-    const stocksByUser = await SSR.DataStore.query(UserStock, (us:any) => us.userId.eq(userDb[0].id));
-    const stocks:any[] = [];
+    const userDb = await SSR.DataStore.query(User, (u: any) => u.sub?.eq(userAuth.attributes.sub));
+    const stocksByUser = await SSR.DataStore.query(UserStock, (us: any) => us.userId.eq(userDb[0].id));
+    const stocks: any[] = [];
     for (const stock of stocksByUser) {
-      let data = await SSR.DataStore.query(Stock, (s:any) => s.id.eq(stock.stockId));
-      stocks.push(data[0]);
+        let data = await SSR.DataStore.query(Stock, (s: any) => s.id.eq(stock.stockId));
+        stocks.push(data[0]);
     }
     return {
         props: {
             user: serializeModel(userDb[0]),
-          stocks: serializeModel(stocks)
+            stocks: serializeModel(stocks)
         }
     }
 }
