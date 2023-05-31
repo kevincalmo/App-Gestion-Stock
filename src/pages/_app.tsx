224222710aeace0e7c21d94f0e7cb563f5a3b1c0
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-31 12:36:53 -0400

    Delete useEffect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,45 +25,8 @@ const theme = extendTheme({
 });
 
 function App({Component, pageProps}: AppProps) {
-    const [isLoading, setIsLoading] = useState(true);
-    const [authUser, setAuthUser] = useState(null);
 
-    useEffect(() => {
-
-        Amplify.Auth.currentAuthenticatedUser()
-            .then((user:any) => user.attributes.sub && setAuthUser(user))
-            .catch((err:any) => console.log(err));
-    }, []);
-
-    useEffect(() => {
-        if (authUser) {
-            (async function() {
-                setIsLoading(true);
-                try {
-                    // @ts-ignore
-                    const userDb = await DataStore.query(User, (u) => u.sub.eq(authUser.attributes.sub));
-                    if (userDb.length === 0) {
-                        await DataStore.save(
-                            new User({
-                                // @ts-ignore
-                                name: authUser.attributes.name,
-                                // @ts-ignore
-                                email: authUser.attributes.email,
-                                isAdmin: false,
-                                // @ts-ignore
-                                sub: authUser.attributes.sub
-                            })
-                        );
-                        console.log("User create !");
-                    }
-                } catch (e) {
-                    console.log("ERROR : " + e);
-                } finally {
-                    setIsLoading(false);
-                }
-            })();
-        }
-    }, [authUser]);
+    
 
 // @ts-ignore
     return <ChakraProvider theme={theme}><StockActiveProvider><Component {...pageProps} /></StockActiveProvider></ChakraProvider>;
