24408f780803584a33e32ed3832fe388c9d9e491
Kévin F. CALMO <kevinfcalmo@gmail.com>
2023-05-31 12:33:54 -0400

    Add size option

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -5,11 +5,12 @@ interface CustomModalProps {
     onClose: any;
     children: any;
     title: string;
+    size?:string;
 }
 
-export default function CustomModal({isOpen, onClose, children, title}: CustomModalProps) {
+export default function CustomModal({isOpen, onClose, children, title, size = "lg"}: CustomModalProps) {
     return (
-        <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false} closeOnEsc={false}>
+        <Modal isOpen={isOpen} size={size} onClose={onClose} closeOnOverlayClick={false} closeOnEsc={false}>
             <ModalOverlay/>
             <ModalContent>
                 <ModalHeader>{title}</ModalHeader>
